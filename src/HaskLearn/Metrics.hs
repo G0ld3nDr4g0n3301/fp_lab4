3404206d115a6ec0fe5417f85760cc7da9f59303
WWamich <admiral3233@mail.ru>
2025-12-17 05:46:52 +0300

    added MSE and MAE

--- src/HaskLearn/Metrics.hs
+++ src/HaskLearn/Metrics.hs
@@ -1,1 +1,9 @@
 module HaskLearn.Metrics where
+
+meanAbsoluteError :: [Double] -> [Double] -> Double
+meanAbsoluteError yTrue yPred = 
+    sum (zipWith (\yt yp -> abs (yt - yp)) yTrue yPred) / fromIntegral (length yTrue)
+
+meanSquaredError :: [Double] -> [Double] -> Double
+meanSquaredError yTrue yPred = 
+    sum (zipWith (\yt yp -> (yt - yp) ^ 2) yTrue yPred) / fromIntegral (length yTrue)
