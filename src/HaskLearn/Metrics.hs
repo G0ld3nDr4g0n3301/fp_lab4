260d254a5970289bf5d813e1475846b5b88aaec2
Денис <wwamich@niuitmo.ru>
2025-12-18 03:09:11 +0300

    all regression metrics are done

--- src/HaskLearn/Metrics.hs
+++ src/HaskLearn/Metrics.hs
@@ -7,3 +7,14 @@ meanAbsoluteError yTrue yPred =
 meanSquaredError :: [Double] -> [Double] -> Double
 meanSquaredError yTrue yPred = 
     sum (zipWith (\yt yp -> (yt - yp) ^ (2 :: Int)) yTrue yPred) / fromIntegral (length yTrue)
+
+rmse :: [Double] -> [Double] -> Double
+rmse yTrue yPred = sqrt (meanSquaredError yTrue yPred)
+
+r2Score :: [Double] -> [Double] -> Double
+r2Score yTrue yPred = 
+    let yMean = sum yTrue / fromIntegral (length yTrue)
+        ssTotal = sum $ map (\y -> (y - yMean) ^ (2 :: Int)) yTrue
+        ssRes   = sum $ zipWith (\yt yp -> (yt - yp) ^ (2 :: Int)) yTrue yPred
+    in 1 - (ssRes / ssTotal)
+
