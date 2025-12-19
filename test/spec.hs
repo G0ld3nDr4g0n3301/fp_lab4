d0c264026c50822744fb61a2d50b44b0431048ec
Денис <wwamich@niuitmo.ru>
2025-12-19 15:35:06 +0300

    added model tests

--- test/spec.hs
+++ test/spec.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE BlockArguments #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
 import Test.Hspec
 import Test.QuickCheck
 import System.Random (mkStdGen)
 import HaskLearn.Metrics
 import HaskLearn.Preprocessing
+import qualified HaskLearn.Models.KNN as KNN
+import qualified HaskLearn.Models.Linreg as Linreg
+import qualified HaskLearn.Models.Logreg as Logreg
+import qualified HaskLearn.Models.Tree as Tree
 
 main :: IO ()
 main = hspec spec
@@ -72,3 +79,41 @@ spec = do
             y2 = take len (ys2 :: [Int])
             res = f1Score (1 :: Int) y1 y2
         in not (null y1) ==> res >= (0.0 :: Double) && res <= (1.0 :: Double)
+    
+  describe "HaskLearn.Models.KNN" $ do
+    it "предсказывает ту же точку с k=1" $ do
+      let trainX = [[1.0, 2.0], [5.0, 6.0], [10.0, 10.0]] :: [[Double]]
+          trainY = [0.0, 1.0, 2.0] :: [Double]
+          model = KNN.fit KNN.Classification KNN.Uniform 1 trainX trainY
+          predictions = KNN.predict model trainX
+      predictions `shouldBe` trainY
+
+  describe "HaskLearn.Models.Linreg" $ do
+    it "обучается на прямой y = 2x" $ do
+      let trainX = [[1.0], [2.0], [3.0]] :: [[Double]]
+          trainY = [2.0, 4.0, 6.0] :: [Double]
+          (model, _) = Linreg.fit trainX trainY 0.01 100 (mkStdGen 42)
+          preds = Linreg.predict model [[4.0]]
+      case preds of
+        (p:_) -> p `shouldSatisfy` (\val -> abs (val - 8.0) < 0.5)
+        []    -> expectationFailure "Linreg no output"
+
+  describe "HaskLearn.Models.Logreg" $ do
+    it "разделяет классы (бинарно)" $ do
+      let trainX = [[1.0, 1.0], [10.0, 10.0]] :: [[Double]]
+          trainY = [0, 1] :: [Int]
+          (model, _) = Logreg.fit trainX trainY 0.1 100 (mkStdGen 42)
+          preds = Logreg.predict model [[1.5, 1.5]]
+      case preds of
+        (p:_) -> p `shouldBe` (0 :: Int)
+        []    -> expectationFailure "Logreg no output"
+
+  describe "HaskLearn.Models.Tree" $ do
+    it "smoke test, т.е возвращает предсказания" $ do
+      let trainX = [[1.0], [2.0], [10.0], [11.0]] :: [[Double]]
+          trainY = [1.0, 1.0, 20.0, 20.0] :: [Double]
+          model = Tree.fit True trainX trainY 5 2
+          preds = Tree.predict model [[1.5]]
+      case preds of
+        (p:_) -> p `shouldSatisfy` (> (0.0 :: Double))
+        []    -> expectationFailure "Tree no output"
